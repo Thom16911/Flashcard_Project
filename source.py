5a6e568a36fc90e732109eecad3cab588fce1883
Thom16911 <128328647+Thom16911@users.noreply.github.com>
2023-03-30 20:54:44 +0100

    Made source code more readable
    Along with adding a few comments for stuff that may not be self-explanatory and removed obsolete code from initial development.

--- source.py
+++ source.py
@@ -9,49 +9,51 @@ def review_button():
     tp = Toplevel()
     tp.title("Flashcard_Studio/Sets")
     tp.geometry("800x350")
-    global res
-    global resSV
-    global g
-    e = 0
-    c = 0
-    res = []
+    global setnames
+    global setnamesStrVar
+    setnames = []
     label2 = Label(tp, text = "Sets:")
     label2.pack()
+    # listdir collects set names to be displayed in OptionMenu() and which when one is selected and the
+    # setselbut Button is clicked will run selectsetoptions()
     for path in os.listdir("flashdata/"):
-        c += 1
-        res.append(path)
-    resSV = StringVar()
-    resSV.set(res[0])
-    dropdownsetsel = ttk.OptionMenu(tp, resSV, *res)
-    setselbut = Button(tp, text = "Set Options:", command = selsetops)
+        setnames.append(path)
+    setnamesStrVar = StringVar()
+    setnamesStrVar.set(setnames[0])
+    dropdownsetsel = ttk.OptionMenu(tp, setnamesStrVar, *setnames)
+    setselbut = Button(tp, text = "Set Options:", command = selectsetoptions)
     dropdownsetsel.pack()
     setselbut.pack()
 
-def selsetops():
-    global g
-    e = resSV.get()
-    with open(f"flashdata/{e}", "r") as f:
-        g = []
-        f.seek(0)
-        g = f.read().splitlines()
-        f.close()
+def selectsetoptions():
+    global selectsetdata
+    filenames = setnamesStrVar.get()
+    # Gets the name of the set picked to be edited
+    with open(f"flashdata/{filenames}", "r") as file:
+        selectsetdata = []
+        file.seek(0)
+        selectsetdata = file.read().splitlines()
+        file.close()
+    # Opens the slected set file and puts all the data into a 1D list formatted [word, defintion]
     tpp = Toplevel()
     tpp.title("Flashcard_Studio/Sets/Mode")
     flashcardmodebutton = Button(tpp, text = "Flashcards", command = flashcardmode)
     flashcardmodebutton.pack()
+    # Currently there is only one option for flashcards available, and that is flashcardmode()
 
 def flashcardmode():
-    global zz
+    global dataindex
     global flipped
     global flashcardflip
     flipped = False
-    zz = 0
-    fw = Toplevel()
-    fw.geometry("1200x625")
-    fw.title("Flashcard_Studio/Sets/Mode/Flashcards")
-    flashcardflip = Button(fw, text = g[zz], height = 10, width = 120, command = flip)
-    prevflashbutton = Button(fw, text = "<", command = prevfl)
-    nextflashbutton = Button(fw, text = ">", command = nextfl)
+    dataindex = 0
+    fm = Toplevel()
+    fm.geometry("1200x625")
+    fm.title("Flashcard_Studio/Sets/Mode/Flashcards")
+    flashcardflip = Button(fm, text = selectsetdata[dataindex], height = 10, width = 120, command = flip)
+    # The flashcards are just a massive button that uses flip() to change the text on the button.
+    prevflashbutton = Button(fm, text = "<", command = prevflash)
+    nextflashbutton = Button(fm, text = ">", command = nextflash)
     flashcardflip.pack()
     prevflashbutton.pack()
     nextflashbutton.pack()
@@ -60,53 +62,47 @@ def flashcardmode():
 def flip():
     global flipped
     if flipped == False:
-        flashcardflip.config(text = g[zz+1])
+        flashcardflip.config(text = selectsetdata[dataindex+1])
+        # Because of the way the list of flashcards is formatted ([word, definition]), the
+        # flashcards are in groups of 2.
         flipped = True
     else:
-        flashcardflip.config(text = g[zz])
+        flashcardflip.config(text = selectsetdata[dataindex])
         flipped = False
 
-def prevfl():
-    global zz
-    zz = zz - 2
-    print(zz)
+def prevflash():
+    global dataindex
+    dataindex = dataindex - 2
+    print(dataindex)
 
-def nextfl():
-    global zz
-    zz = zz + 2
-    print(zz)
+def nextflash():
+    global dataindex
+    dataindex = dataindex + 2
+    print(dataindex)
 
 def scrape():
-    flshlinelen = 0
-    print("Button pressed")
     HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
     }
     soup = BeautifulSoup(requests.get(entry_text.get(), headers=HEADERS).text, "html.parser")
+    # Gets the actual HTML file of the Quizlet that you would like to scrape.
     flashcard_title = soup.find("h1").text
 
-    f = open(f'flashdata/{flashcard_title}.txt', 'w')
+    file = open(f'flashdata/{flashcard_title}.txt', 'w')
+    # Creates and opens a new file or opens an old one ready for re/writing
 
-    flsh = soup.find_all("div", class_ = "SetPageTerms-term")
-    flsh += soup.find_all("div", class_ = "SetPageTerms-term SetPageTerms-term--beyondSignupThreshold")
+    setdata = soup.find_all("div", class_ = "SetPageTerms-term")
+    setdata += soup.find_all("div", class_ = "SetPageTerms-term SetPageTerms-term--beyondSignupThreshold")
+    # Collecting the setdata divs, not the raw data.
 
-    for index, flashcards in enumerate(flsh):
-        flshlst = flsh[index]
-        flash1 = flshlst.find("a", class_ = "SetPageTerm-wordText").text
-        flash2 = flshlst.find("a", class_ = "SetPageTerm-definitionText").text
-        with open(f"flashdata/{flashcard_title}.txt", "a") as f:
-            f.write(f"w: {flash1}\n")
-            f.write(f"def: {flash2}\n")
-            flshlinelen += 2
-            print(f"w: {flash1}\n")
-            print(f"def: {flash2}\n")
-    opener = open("flashdata/" + flashcard_title + ".txt", "a")
-    opener.write(str(flshlinelen))
-    print(flshlinelen)
-    opener.close()
-    f.close()
-    x = open(f"flashdata/{flashcard_title}.txt", "r")
-    print(x.read())
+    for index, flashcards in enumerate(setdata):
+        flashlist = setdata[index]
+        flashword = flashlist.find("a", class_ = "SetPageTerm-wordText").text
+        flashdefinition = flashlist.find("a", class_ = "SetPageTerm-definitionText").text
+        with open(f"flashdata/{flashcard_title}.txt", "a") as file:
+            file.write(f"w: {flashword}\n")
+            file.write(f"def: {flashdefinition}\n")
+    file.close()
 
 root = Tk()
 
